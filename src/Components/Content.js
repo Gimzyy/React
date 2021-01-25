1a7ae60877f7058518ff9cacc060f4aba4f39ec7
Gimzyy <prestigio5501dima@gmail.com>
2021-01-25 15:39:15 +0300

    new componets

---
+++ src/Components/Content.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Content = ()=> {
+    return(
+        <div className= 'section-content'>
+            <div>
+                <img src= 'https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg' />
+            </div>
+            <div>
+                img + info
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Content;
